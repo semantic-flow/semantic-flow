694a2f8d29fde8b1fafbc67b814772653a8f05da
Dave Richardson <djradon@users.noreply.github.com>
2025-07-22 17:38:06 -0700

    updated constants

--- flow-core/src/mesh-constants.ts
+++ flow-core/src/mesh-constants.ts
@@ -1,31 +1,33 @@
 // flow-core/src/constants.ts
 export const MESH_CONSTANTS = {
-  // Component directory names
-  CONFIG_COMPONENT: "_config-component",
-  META_COMPONENT: "_meta-component",
-  ASSETS_COMPONENT: "_assets",
-  NEXT_LAYER: "_next",
-  CURRENT_LAYER: "_current",
+  // Flow directory names
+  CONFIG_FLOW_DIR: "_config-flow",
+  META_FLOW_DIR: "_meta-flow",
+  REFERENCE_FLOW_DIR: "_ref-flow",
+  DATA_FLOW_DIR: "_data-flow",
 
-  // Versioning directories
-  VERSION_PREFIX: "_v",
-  VERSION_SERIES: "_v-series",
-  REFERENCE_DIR: "_ref",
+  // Other element directory names
   HANDLE_DIR: "_handle",
+  ASSETS_COMPONENT_DIR: "_assets",
+  NEXT_SNAPSHOT_DIR: "_next",
+  CURRENT_SNAPSHOT_DIR: "_current",
+  VERSION_PREFIX: "_v",
 
   // System files
-  CONFIG_DEFAULTS_FILE: "flow-config-defaults.jsonld",
   CONFIG_FILE: "flow-config.jsonld",
   README_FILE: "README.md",
-
+  CHANGELOG_FILE: "CHANGELOG.md",
 
   // Ontology namespaces
+  MESH_ONTOLOGY: "https://semantic-flow.github.io/ontology/mesh/",
+  NODE_ONTOLOGY: "https://semantic-flow.github.io/ontology/node/",
   FLOW_ONTOLOGY: "https://semantic-flow.github.io/ontology/flow/",
-  SERVICE_ONTOLOGY: "https://semantic-flow.github.io/ontology/flow-service/",
-  NODE_ONTOLOGY: "https://semantic-flow.github.io/ontology/node-config/"
+  CONFIG_FLOW_ONTOLOGY: "https://semantic-flow.github.io/ontology/config-flow/",
+  META_FLOW_ONTOLOGY: "https://semantic-flow.github.io/ontology/meta-flow/",
+  FLOW_SERVICE_ONTOLOGY: "https://semantic-flow.github.io/ontology/flow-service/"
 } as const;
 
 // Usage throughout codebase
 export function getNodeConfigPath(nodePath: string): string {
-  return `${nodePath}/${MESH_CONSTANTS.CONFIG_COMPONENT}`;
+  return `${nodePath}/${MESH_CONSTANTS.CONFIG_FLOW}`;
 }
